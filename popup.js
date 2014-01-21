b36ff4fd6eabc72054ad04c3cacf21c1cd7b2be8
horzwxy <wangxiayang.fdt@gmail.com>
2014-01-22 01:34:45 +0800

    complete first usable

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
 function getElementByClass(className) {
 	var elems = document.getElementsByTagName('*'), i;
-    for (i in elems) {
-        if((' ' + elems[i].className + ' ').indexOf(' ' + className + ' ')
-                > -1) {
-        	return elems[i];
-        }
-    }
+	for (i in elems) {
+		if((' ' + elems[i].className + ' ').indexOf(' ' + className + ' ')
+			> -1) {
+			return elems[i];
+	}
+}
 }
 
 function addButton() {
@@ -26,32 +26,58 @@ function addListener() {
 		a.initEvent("click", true, true);
 		button.getclass("fp-direction fp-prev ").dispatchEvent(a);
 	}
+	button.checkId = function() {
+		if(button.loopIndex >= 1000) {
+			window.clearInterval(button.intervalId);
+			alert("not found after " + button.loopIndex + " prevs");
+			return;
+		}
+
+		prevLink = button.getclass("fp-direction fp-prev ");
+		if(!prevLink) {
+			alert("prevLink not found after " + button.loopIndex + " prevs");
+			window.clearInterval(button.intervalId);
+			return;
+		}
+		else {
+			var photoLink = button.getclass("fp-stage clearfix");
+			if(!photoLink) {
+				alert("photoLink not found");
+				window.clearInterval(button.intervalId);
+				return;
+			}
+			else {
+				var url = photoLink.href;
+				if(url == button.lastURL) {
+					//alert("equal url after " + button.loopIndex + "prevs");
+				}
+				else {
+					button.lastURL = url;
+				}
+				var ownerId = url.split("/")[4];
+				var userId = document.URL.split("/")[3];
+				if(ownerId - userId) {
+					button.loopIndex++;
+					return;
+				}
+				else {
+					// equivalent ids found
+					alert("found your photo after " + button.loopIndex + " prevs");
+					window.clearInterval(button.intervalId);
+					return;
+				}
+			}
+		}
+	}
 	button.onclick = function(e) {
 		var prevLink = button.getclass("fp-direction fp-prev ");
 		if (!prevLink) {
 			alert("prevLink not found!");
 		}
 		else {
-			var i = 0;
-			while(i < 100) {
-				i++;
-				button.trigger();
-				prevLink = button.getclass("fp-direction fp-prev ");
-				if(!prevLink) {
-					alert("prevLink not found after " + i + " prevs");
-				}
-				else {
-					var photoLink = button.getclass("fp-stage clearfix");
-					if(!photoLink) {
-						alert("photoLink not found");
-					}
-					else {
-						alert(photoLink.href);
-						alert("succeed");
-					}
-					break;
-				}
-			}
+			button.loopIndex = 0;
+			var intervalId = window.setInterval("document.getElementById(\"showPhotos\").trigger();document.getElementById(\"showPhotos\").checkId()", 500);
+			button.intervalId = intervalId;
 		}
 	}
 }
